e009548ebb05f053b03fc5cfb3ab43694fbbfd53
geomonitora@gmail.com <geomonitora@gmail.com>
2023-05-15 10:37:15 -0300

    Aula 3

--- src/componentes/Formulario/index.tsx
+++ src/componentes/Formulario/index.tsx
@@ -1,16 +1,22 @@
-import "./Formulario.css";
+import { useState } from "react";
+import { IColaborador } from "../../compartilhado/interface/IColaborador";
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
-import Botao from "../Botao";
-import { useState } from "react";
+import "./Formulario.css";
 
-const Formulario = (props) => {
+interface FormularioProps {
+  aoColaboradorCadastrado: (colaborador: IColaborador) => void;
+  times: string[];
+}
+
+const Formulario = (props: FormularioProps) => {
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
   const [imagem, setImagem] = useState("");
-  const [time, setTime] = useState();
+  const [time, setTime] = useState("");
 
-  const aoSalvar = (evento) => {
+  const aoSalvar = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
     props.aoColaboradorCadastrado({
       nome,
@@ -26,19 +32,19 @@ const Formulario = (props) => {
 
   return (
     <section className="formulario">
-      <form onSubmit={aoSalvar}>
+      <form onSubmit={(evento) => aoSalvar(evento)}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto
           obrigatorio={true}
           label="Nome"
-          placeholder="Digite o seu nome"
+          placeholder="Digite seu nome"
           valor={nome}
           aoAlterado={(valor) => setNome(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Cargo"
-          placeholder="Digite o seu cargo"
+          placeholder="Digite seu cargo"
           valor={cargo}
           aoAlterado={(valor) => setCargo(valor)}
         />
